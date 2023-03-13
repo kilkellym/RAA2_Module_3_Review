9cf41b46415cc4700c6e6190e40bb4738198ccf4
kilkellym <michael@archsmarter.com>
2023-03-13 15:34:13 -0400

    Add project files.

---
+++ RAA2_Module_3_Review/Forms/MyForm.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace RAA2_Module_3_Review
+{
+    /// <summary>
+    /// Interaction logic for Window.xaml
+    /// </summary>
+    public partial class MyForm : Window
+    {
+        ObservableCollection<SheetData> SheetList { get; set; }
+        public MyForm()
+        {
+            InitializeComponent();
+
+            SheetList = new ObservableCollection<SheetData>();
+
+            SheetGrid.DataContext = SheetList;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
---
+++ RAA2_Module_3_Review/SheetData.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAA2_Module_3_Review
+{
+    internal class SheetData
+    {
+        public string SheetNumber { get; set; }
+        public string SheetName { get; set;}
+        public bool IsPlaceholder { get; set; }
+        public Element Titleblock { get; set; }
+    }
+}
